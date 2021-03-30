6c448ec50d7fd251e745264136392f8f05432db0
shohan98 <dshohan112@gmail.com>
2021-03-30 23:55:54 +0600

    prepare boneadmin.models.py and boneadmin.admin.py file

--- project/BONE/bone71/Boneadmin/admin.py
+++ project/BONE/bone71/Boneadmin/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from .models import (YoutubeContent, VideoAd, VideoAdWatchMonthlyReport,
+                     HorizontalBannerAd, HorizontalAdWatchMonthlyReport,
+                     VerticalBannerAd, VerticalAdWatchMonthlyReport,
+                     ContentCategory, ContentClickMonthlyReport)
 
 # Register your models here.
+
+
+class YoutubeContentAdmin(admin.ModelAdmin):
+    list_display = ['content_name', 'category', 'total_click', 'status',
+                    'created_at']
+    search_fields = ['content_name']
+    list_filter = ('category',)
+
+class VideoAdAdmin(admin.ModelAdmin):
+    list_display = ['video_name', 'total_watch', 'status']
+    search_fields = ['video_name',]
+
+class VideoAdWatchMonthlyReportAdmin(admin.ModelAdmin):
+    list_display = ['content', 'report_date', 'total_watch']
+    search_fields = ['content', 'report_date']
+
+class HorizontalBannerAdAdmin(admin.ModelAdmin):
+    list_display = ['banner_name', 'target_link', 'total_watch', 'status', 
+                    'position', 'created_at']
+    search_fields = ['banner_name',]
+
+class HorizontalAdWatchMonthlyReportAdmin(admin.ModelAdmin):
+    list_display = ['content', 'report_date', 'total_watch', 'total_click']
+    search_fields = ['content', 'report_date']
+
+class VerticalBannerAdAdmin(admin.ModelAdmin):
+    list_display = ['banner_name', 'target_link', 'total_watch', 'status', 
+                    'position', 'created_at']
+    search_fields = ['banner_name',]
+
+class VerticalAdWatchMonthlyReportAdmin(admin.ModelAdmin):
+    list_display = ['content', 'report_date', 'total_watch', 'total_click']
+    search_fields = ['content', 'report_date']
+
+class ContentCategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name', 'total_video', 'total_click']
+    search_fields = ['category_name',]
+
+class ContentClickMonthlyReportAdmin(admin.ModelAdmin):
+    list_display = ['content', 'report_date', 'total_click']
+    search_fields = ['content', 'report_date']
+
+admin.site.register(YoutubeContent, YoutubeContentAdmin)
+admin.site.register(VideoAd, VideoAdAdmin)
+admin.site.register(VideoAdWatchMonthlyReport, VideoAdWatchMonthlyReportAdmin)
+admin.site.register(HorizontalBannerAd, HorizontalBannerAdAdmin)
+admin.site.register(HorizontalAdWatchMonthlyReport,HorizontalAdWatchMonthlyReportAdmin)
+admin.site.register(VerticalBannerAd, VerticalBannerAdAdmin)
+admin.site.register(VerticalAdWatchMonthlyReport, VerticalAdWatchMonthlyReportAdmin)
+admin.site.register(ContentCategory, ContentCategoryAdmin)
+admin.site.register(ContentClickMonthlyReport, ContentClickMonthlyReportAdmin)
--- project/BONE/bone71/Boneadmin/models.py
+++ project/BONE/bone71/Boneadmin/models.py
@@ -1,3 +1,102 @@
 from django.db import models
-
+from django.utils import timezone as tz
 # Create your models here.
+
+class ContentCategory(models.Model):
+    category_name = models.CharField(max_length=60)
+    total_video   = models.IntegerField(default=0)
+    total_click   = models.IntegerField(default=0)
+    added_by_ip   = models.CharField(max_length=25)
+    created_at    = models.DateTimeField(default=tz.now)
+    
+    def __str__(self):
+        return self.category_name
+    
+
+class YoutubeContent(models.Model):
+    content_name    = models.CharField(max_length=600)
+    category        = models.ForeignKey(ContentCategory, on_delete=models.CASCADE)
+    content_link    = models.URLField()
+    total_click     = models.IntegerField(default=0)
+    status          = models.BooleanField(default=True)
+    added_by_ip     = models.CharField(max_length=25)
+    created_at      = models.DateTimeField(default=tz.now)
+    
+    def __str__(self):
+        return self.content_name
+
+class VideoAd(models.Model):
+    video_name      = models.CharField(max_length=300)
+    video           = models.FileField()
+    total_watch     = models.IntegerField(default=0)
+    status          = models.BooleanField(default=True)
+    added_by_ip     = models.CharField(max_length=25)
+    created_at      = models.DateTimeField(default=tz.now)
+    
+    def __str__(self):
+        return self.video_name
+
+class VerticalBannerAd(models.Model):
+    banner_name     = models.CharField(max_length=300)
+    banner          = models.FileField()
+    target_link      = models.URLField(null=True)
+    total_watch     = models.IntegerField(default=0)
+    status          = models.BooleanField(default=True)
+    position        = models.IntegerField(default=0)
+    added_by_ip     = models.CharField(max_length=25)
+    created_at      = models.DateTimeField(default=tz.now)
+    
+    def __str__(self):
+        return self.banner_name
+
+class HorizontalBannerAd(models.Model):
+    banner_name      = models.CharField(max_length=300)
+    banner           = models.FileField()
+    target_link      = models.URLField(null=True)
+    total_watch     = models.IntegerField(default=0)
+    status          = models.BooleanField(default=True)
+    position        = models.IntegerField(default=0)
+    added_by_ip     = models.CharField(max_length=25)
+    created_at      = models.DateTimeField(default=tz.now)
+    
+    def __str__(self):
+        return self.banner_name
+
+class ContentClickMonthlyReport(models.Model):
+    content         = models.ForeignKey(YoutubeContent, on_delete=models.CASCADE)
+    report_date     = models.CharField(max_length=30)
+    total_click     = models.IntegerField(default=0)
+    created_at      = models.DateTimeField(default=tz.now)
+    
+    def __str__(self):
+        return self.report_date
+
+class VideoAdWatchMonthlyReport(models.Model):
+    content         = models.ForeignKey(VideoAd, on_delete=models.CASCADE)
+    report_date     = models.CharField(max_length=30)
+    total_watch     = models.IntegerField(default=0)
+    created_at      = models.DateTimeField(default=tz.now)
+    
+    def __str__(self):
+        return self.report_date
+
+class VerticalAdWatchMonthlyReport(models.Model):
+    content         = models.ForeignKey(VerticalBannerAd, on_delete=models.CASCADE)
+    report_date     = models.CharField(max_length=30)
+    total_watch     = models.IntegerField(default=0)
+    total_click     = models.IntegerField(default=0)
+    created_at      = models.DateTimeField(default=tz.now)
+    
+    def __str__(self):
+        return self.report_date
+
+class HorizontalAdWatchMonthlyReport(models.Model):
+    content         = models.ForeignKey(HorizontalBannerAd, on_delete=models.CASCADE)
+    report_date     = models.CharField(max_length=30)
+    total_watch     = models.IntegerField(default=0)
+    total_click     = models.IntegerField(default=0)
+    created_at      = models.DateTimeField(default=tz.now)
+    
+    def __str__(self):
+        return self.report_date
+
